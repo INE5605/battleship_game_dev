7eb71098e0c613f9af629b08073634fedc6d8a68
Alexandre Conte <alexandreconte.dev@gmail.com>
2023-10-06 16:53:24 -0300

    implementado controlador e tela de jogador

---
+++ jogador/controlador_jogador.py
@@ -0,0 +1,70 @@
+from jogador.jogador import Jogador
+from jogador.tela_jogador import TelaJogador
+
+
+class CtrlJogador:
+    # def __init__(self, controlador_principal) -> None:
+    def __init__(self) -> None:
+        self.__tela_jogador = TelaJogador()
+        # self.__controlador_principal = controlador_principal
+        self.__jogadores = []
+
+    @property
+    def tela_jogador(self):
+        return self.__tela_jogador
+
+    @property
+    def controlador_principal(self):
+        return self.__controlador_principal
+
+    @property
+    def jogadores(self) -> list:
+        return self.__jogadores
+
+    @jogadores.setter
+    def jogadores(self, jogadores) -> None:
+        self.__jogadores = jogadores
+
+    def cadastra_jogador(self):
+        dados_jogador = self.tela_jogador.adiciona_jogador()
+        nome = dados_jogador["nome"]
+        data_nasc = dados_jogador["data_nasc"]
+        jogador = Jogador(nome=nome, data_nasc=data_nasc)
+        self.jogadores.append(jogador)
+        self.tela_jogador.escreve_mensagem("Jogador Cadastrado!")
+
+    def __pega_jogador_por_nome(self, nome: str) -> Jogador:
+        for jogador in self.jogadores:
+            if jogador.nome == nome:
+                return jogador
+        return None
+
+    def remove_jogador(self):
+        dados_jogador = self.tela_jogador.remocao_jogador()
+        nome = dados_jogador["nome"]
+        jogador = self.__pega_jogador_por_nome(nome)
+        self.jogadores.remove(jogador)
+        self.tela_jogador.escreve_mensagem("Jogador removido!")
+
+    def lista_jogadores(self):
+        self.tela_jogador.lista_jogador(self.jogadores)
+
+    def altera_jogador(self):
+        dados_jogador = self.tela_jogador.edita_nome()
+        nome_antigo = dados_jogador["nome_antigo"]
+        nome_novo = dados_jogador["nome_novo"]
+        jogador = self.__pega_jogador_por_nome(nome_antigo)
+        jogador.nome = nome_novo
+        self.tela_jogador.escreve_mensagem("Jogador Alterado!")
+
+    def abre_tela(self):
+        menu = {
+            1: self.cadastra_jogador,
+            2: self.altera_jogador,
+            3: self.lista_jogadores,
+            4: self.remove_jogador,
+            # 0: self.controlador_principal.tela_opcoes
+        }
+        opcao = self.tela_jogador.tela_opcoes()
+        menu[opcao]()
+        # self.controlador_principal.abre_tela()
---
+++ jogador/tela_jogador.py
@@ -0,0 +1,64 @@
+from datetime import datetime
+
+
+class TelaJogador:
+    def tela_opcoes(self):
+        print("--- Tela ---")
+        print("Escolha a opcao")
+        print("1 - Adicionar jogador")
+        print("2 - Alterar jogador")
+        print("3 - Listar jogadores")
+        print("4 - Excluir jogador")
+        print("0 - Retornar")
+
+        opcoes_validas = [0, 1, 2, 3, 4]
+
+        while True:
+            try:
+                opcao = int(input("Escolha a opcao: "))
+                if opcao not in opcoes_validas:
+                    raise ValueError(
+                        "Opcao invalida, digite uma opcao valida."
+                    )
+                return opcao
+            except ValueError:
+                print("Digite apenas o numero da opcao escolhida.")
+
+    def adiciona_jogador(self) -> dict[str, datetime]:
+        nome = input("Digite o seu nome: ")
+        while True:
+            data_nasc = input("Digite a sua data de nascimento (dd/mm/yyyy): ")
+            try:
+                data_nasc = datetime(
+                    year=int(data_nasc.split("/")[2]),
+                    month=int(data_nasc.split("/")[1]),
+                    day=int(data_nasc.split("/")[0])
+                )
+                return {
+                    "nome": nome,
+                    "data_nasc": data_nasc
+                }
+            except ValueError:
+                print("Data invalida")
+
+    def edita_nome(self):
+        nome_antigo = input("Digite o nome do jogador: ")
+        nome_novo = input("Digite o novo nome do jogador: ")
+        return {
+            "nome_antigo": nome_antigo,
+            "nome_novo": nome_novo
+        }
+
+    def remocao_jogador(self):
+        nome = input("Digite o nome do jogador a ser removido: ")
+        return {
+            "nome": nome
+        }
+
+    def lista_jogador(self, jogadores):
+        print("Jogadores Cadastrados no Sistema:\n")
+        for jogador in jogadores:
+            print(jogador + "\n")
+
+    def escreve_mensagem(mensagem: str) -> None:
+        print(mensagem)
