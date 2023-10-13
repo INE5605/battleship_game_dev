3cfe918a0270587f1237cd842a48e661ee499d4c
felipedelduqui <felipedelduqui@hotmail.com>
2023-10-13 19:59:21 -0300

    embarcacao e partida atualizadas

--- embarcacao/embarcacao.py
+++ embarcacao/embarcacao.py
@@ -1,17 +1,23 @@
 class Embarcacao:
-	def __init__(self, tamanho):
+	def __init__(self, tamanho: int):
 
 		self.__tamanho = tamanho
-		self.__atingido = [False]*tamanho
+		self.__shield = tamanho
 
-		def afundou(self):
-			# Verifica se a embarcação foi afundada (todas as partes atingidas)
+	def afundou(self):
+		'''Verifica se a embarcação foi afundada (todas as partes atingidas)'''
 
-			return all(self.__atingido)
+		return self.__shield == 0
 
-		def recebe_ataque(self, coordenada):
-			# Recebe um ataque em uma coordenada
-			# Atualiza self.atingido para marcar a parte da embarcação atingida
+	def recebe_ataque(self, coordenada) -> bool:
+		'''Recebe um ataque em uma coordenada
+		Atualiza self.shield e retorna True se afundou
+		ou False se não afundou'''
 
-			self.__atingido[coordenada] = True
-			return None
+		self.__shield -= 1
+
+		print("Embarcação atacada. Registrar 1 ponto")
+
+		if self.afundou():
+			print("Embarcação afundou. Registrar 3 pontos")
+		return self.afundou()
