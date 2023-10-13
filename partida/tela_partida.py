06948034404f6470b6fe4ecf4e71db4144b340a8
Felipe Delduqui <felipedelduqui@hotmail.com>
2023-10-13 15:47:20 -0300

    deletando pycache (#3)

--- partida/tela_partida.py
+++ partida/tela_partida.py
@@ -3,7 +3,7 @@ class TelaPartida():
         pass
 
 
-    def tela_opcoes(self) -> int:
+    def tela_opcoes_tela_novo_jogo(self) -> int:
         print("--- Tela ---")
         print("Escolha a opcao")
         print("1 - Novo jogador")
@@ -11,6 +11,26 @@ class TelaPartida():
         print("0 - Voltar")
 
         opcoes_validas = [0, 1, 2]
+
+        while True:
+            try:
+                opcao = int(input("Escolha a opcao: "))
+                if opcao not in opcoes_validas:
+                    raise ValueError(
+                        "Opcao invalida, digite uma opcao valida."
+                    )
+                return opcao
+            except ValueError:
+                print("Digite apenas o numero da opcao escolhida.")
+
+    def tela_opcoes_tela_partida(self) -> int:
+
+        print("--- Tela ---")
+        print("Escolha a opcao")
+        print("1 - Bombardear computador")
+        print("0 - Desistir")
+
+        opcoes_validas = [0, 1]
 
         while True:
             try:
