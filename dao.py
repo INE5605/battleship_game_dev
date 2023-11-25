7081bb2e3c2c179e8d09aed49297903d22d121e0
Alexandre Conte <alexandreconte.dev@gmail.com>
2023-11-25 16:25:46 -0300

    Implementando persistencia de partidas

--- dao.py
+++ dao.py
@@ -2,6 +2,7 @@ from objeto_nao_existe_exception import ObjetoNaoExisteException
 import pickle
 from abc import ABC, abstractmethod
 
+
 class DAO(ABC):
     @abstractmethod
     def __init__(self, datasource='') -> None:
@@ -11,26 +12,26 @@ class DAO(ABC):
             self.__load()
         except FileNotFoundError:
             self.__dump()
-    
+
     def __dump(self):
         pickle.dump(self.__cache, open(self.__datasource, 'wb'))
-        
+
     def __load(self):
         self.__cache = pickle.load(open(self.__datasource, 'rb'))
-        
+
     def add(self, key, obj):
         self.__cache[key] = obj
         self.__dump()
-        
+
     def get(self, key):
         try:
             return self.__cache[key]
         except KeyError:
             return None
-        
+
     def get_all(self):
         return self.__cache.values()
-    
+
     def remove(self, key):
         try:
             self.__cache.pop(key)
